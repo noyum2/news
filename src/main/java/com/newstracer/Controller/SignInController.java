7329ffaaa68e3c2f686d1f7497016c9d00b10887
noyum2 <noyum2@naver.com>
2016-11-06 17:28:08 +0900

    로그인

---
+++ src/main/java/com/newstracer/Controller/SignInController.java
@@ -0,0 +1,32 @@
+package com.newstracer.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.newstracer.Service.UserService;
+import com.newstracer.VO.Base;
+import com.newstracer.VO.User;
+
+@Controller
+@RequestMapping("/signin")
+public class SignInController {
+	
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(value="/",method = RequestMethod.POST)
+	public @ResponseBody User SignIn(@RequestBody HttpServletRequest request,HttpSession session)
+	{
+		User user = userService.LoginUser(request.getParameter("id"), request.getParameter("pw"));
+		if(user.getResultCode()==200)
+			session.setAttribute("user", user);
+		return user;
+	}
+}
